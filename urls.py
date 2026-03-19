1f1c90102d3d0bed4f084b686b68b6a582f42635
Deolyn East <deolyneast@gmail.com>
2026-03-19 11:46:40 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,12 +3,8 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    # ... your existing paths (index, course_details, etc.)
-    
-    # TASK 6: Submit path
+    # ... other paths ...
     path('<int:course_id>/submit/', views.submit, name='submit'),
-    
-    # TASK 6: Show Exam Result path (Must include submission_id)
     path('course/<int:course_id>/submission/<int:submission_id>/result/', 
          views.show_exam_result, name='show_exam_result'),
 ]
